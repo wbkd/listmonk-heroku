91a926c6e8d81042cff2ad8ba35cec2af6edf2c4
moklick <info@moritzklack.com>
2021-03-04 18:03:02 +0100

    fix(start.sh): add app-init

--- start-listmonk.sh
+++ start-listmonk.sh
@@ -5,6 +5,9 @@ if [ ! -f config/listmonk.toml ]; then
   erb config/listmonk.toml.erb > config/listmonk.toml
 fi
 
+
+touch /tmp/app-initialized
+
 # Update and start
 ./listmonk --config=config/listmonk.toml --upgrade --yes
 ./listmonk --config=config/listmonk.toml
